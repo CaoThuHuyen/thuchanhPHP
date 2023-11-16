7fa6638453eafc768bc30c7db83a922a1c074d48
CaoThuHuyen <144611621+CaoThuHuyen@users.noreply.github.com>
2023-11-16 16:17:47 +0700

    Update check.php

--- ajax/DanhNhapAjax/check.php
+++ ajax/DanhNhapAjax/check.php
@@ -12,7 +12,7 @@ if(mysqli_num_rows($rs)>0){
     $row = mysqli_fetch_array($rs);
     $_SESSION["code"]=$row['masv'];
     $_SESSION["info"]=$us."-".$row["hoten"]."-".$row["lop"];
-    echo $us."-".$row['hoten']."$nbsp;<a href = QLSV.php>Next</a>";}
+    echo $us."-".$row['hoten']."$nbsp;|$nbsp;<a href = QLSV.php>Next</a>";}
     else{
         echo "Bạn nhập thông tin sai!";
         mysqli_free_result($rs);
